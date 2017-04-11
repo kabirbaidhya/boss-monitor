31cce8d0ae373a3a7314c0de64995143cb489359
Saugat Acharya <mesaugat@gmail.com>
2017-04-11 21:45:44 +0545

    Remove new lines

--- test/services/notify.test.js
+++ test/services/notify.test.js
@@ -4,7 +4,6 @@ import * as slack from '../../src/services/slack';
 import * as notifier from '../../src/services/notifier';
 
 describe('Notifier', () => {
-
   it('should notify if slack is enabled', () => {
     let isEnabled = sinon.stub(slack, 'isEnabled').returns(true);
     let notify = sinon.spy(slack, 'notify');
@@ -17,5 +16,4 @@ describe('Notifier', () => {
 
     isEnabled.restore();
   });
-
 });
