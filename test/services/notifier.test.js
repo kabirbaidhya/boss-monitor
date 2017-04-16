d36a9f1b1f3c255ebfa6b2f2c44a36e71935f9c6
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-04-16 20:55:55 +0545

    Fix the TypeError on the notifier test

--- test/services/notifier.test.js
+++ test/services/notifier.test.js
@@ -1,13 +1,14 @@
 import sinon from 'sinon';
 import { assert } from 'chai';
+import { STATUS_UP } from '../../src/services/status';
 import * as slack from '../../src/services/slack';
 import * as notifier from '../../src/services/notifier';
 
-describe('Notifier', () => {
+describe('notifier', () => {
   it('should notify if slack is enabled', () => {
     let isEnabled = sinon.stub(slack, 'isEnabled').returns(true);
     let notify = sinon.spy(slack, 'notify');
-    let params = { some: 'thing' };
+    let params = { status: STATUS_UP, name: 'foo' };
 
     notifier.notify(params);
 
