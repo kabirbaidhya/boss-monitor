8bbd60632760ed89f8ff02e9aea0e9479e516cb0
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-04-24 22:57:16 +0545

    Fix an issue due to shallow config merging with Object.assign()

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,14 @@
 import Yaml from 'yamljs';
 import Promise from 'bluebird';
+import merge from 'webpack-merge';
 import defaultConfig from './default.config';
 
 global.Promise = Promise;
 
 // TODO: Need a way to specify config file from CLI option.
+// TODO: Currently the config merging stuff is not testable, need to make it testable.
 const CONFIG_FILE_NAME = (process.env.NODE_ENV === 'test') ? 'chill.test.yml' : 'chill.yml';
 const loadedConfig = Yaml.load(CONFIG_FILE_NAME);
-const config = Object.assign({}, defaultConfig, loadedConfig);
+const config = merge(defaultConfig, loadedConfig);
 
 export default config;
