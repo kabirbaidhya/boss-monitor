df054d6c118d1b7f9d14e70d61acfb5dc4a96e3b
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-01 16:51:34 +0545

    Refactor configuration

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,30 @@ global.Promise = Promise;
 // TODO: Need a way to specify config file from CLI option.
 // TODO: Currently the config merging stuff is not testable, need to make it testable.
 const CONFIG_FILE_NAME = (process.env.NODE_ENV === 'test') ? 'chill.test.yml' : 'chill.yml';
-const loadedConfig = Yaml.load(CONFIG_FILE_NAME);
-const config = merge(defaultConfig, loadedConfig);
 
-export default config;
+const DEFAULT_FILENAME = 'chill.yml';
+
+let resolvedConfig = {};
+
+/**
+ * Resolve configuration by reading the configuration file.
+ * 
+ * @param {string} [filename=DEFAULT_FILENAME] 
+ * @returns {Object}
+ */
+export function resolve(filename = DEFAULT_FILENAME) {
+  const loadedConfig = Yaml.load(filename);
+
+  resolvedConfig = merge(defaultConfig, loadedConfig);
+
+  return resolvedConfig;
+}
+
+/**
+ * Return the resolved configuration.
+ * 
+ * @returns {Object}
+ */
+export function get() {
+  return resolvedConfig;
+}
