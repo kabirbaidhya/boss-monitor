6bb512dd72485d96bee3027613cb09a9965aaef4
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-01 22:01:31 +0545

    Setup tests for configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,13 @@
 import Yaml from 'yamljs';
 import Promise from 'bluebird';
+import cache from 'memory-cache';
 import merge from 'webpack-merge';
 import defaultConfig from './default.config';
 
 global.Promise = Promise;
 
-// TODO: Need a way to specify config file from CLI option.
-// TODO: Currently the config merging stuff is not testable, need to make it testable.
-const CONFIG_FILE_NAME = (process.env.NODE_ENV === 'test') ? 'chill.test.yml' : 'chill.yml';
-
-const DEFAULT_FILENAME = 'chill.yml';
-
-let resolvedConfig = {};
+export const CACHE_KEY = 'config';
+export const DEFAULT_FILENAME = 'chill.yml';
 
 /**
  * Resolve configuration by reading the configuration file.
@@ -20,11 +16,13 @@ let resolvedConfig = {};
  * @returns {Object}
  */
 export function resolve(filename = DEFAULT_FILENAME) {
-  const loadedConfig = Yaml.load(filename);
+  let loadedConfig = Yaml.load(filename);
+  let config = merge(defaultConfig, loadedConfig);
 
-  resolvedConfig = merge(defaultConfig, loadedConfig);
+  // Put the resolved config into the cache.
+  cache.put(CACHE_KEY, config);
 
-  return resolvedConfig;
+  return config;
 }
 
 /**
@@ -33,5 +31,5 @@ export function resolve(filename = DEFAULT_FILENAME) {
  * @returns {Object}
  */
 export function get() {
-  return resolvedConfig;
+  return cache.get(CACHE_KEY);
 }
---
+++ test/config/config.get.test.js
@@ -0,0 +1,30 @@
+import sinon from 'sinon';
+import faker from 'faker';
+import { assert } from 'chai';
+import cache from 'memory-cache';
+import * as config from '../../src/config/config';
+
+describe('config.get', () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should return the previously resolved config from the cache.', () => {
+    let resolvedConfig = {
+      foo: faker.random.word()
+    };
+
+    sandbox.stub(cache, 'get').callsFake(key => {
+      assert.equal(key, config.CACHE_KEY);
+
+      return resolvedConfig;
+    });
+    assert.deepEqual(config.get(), resolvedConfig);
+  });
+});
