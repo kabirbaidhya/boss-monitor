4829c295710d813759aa6b84461c116207a7e7f8
pratishshr <vanroshr@gmail.com>
2017-05-15 11:41:08 +0545

    Implement dao layer for services

---
+++ src/daos/serviceDao.js
@@ -0,0 +1,60 @@
+import logger from '../utils/logger';
+import Service from '../models/Service';
+
+/**
+ * Fetch a service by its name.
+ *
+ * @param  {number|string}  id
+ * @return {Promise}
+ */
+export async function fetchByName(name) {
+  try {
+    return await
+      new Service({ name })
+        .fetch()
+        .then(service => {
+          if (!service) {
+            return null;
+          }
+
+          return service;
+        });
+  } catch (err) {
+    logger.error('Error fetching service from database.', err);
+  }
+}
+
+/**
+ * Create a new service.
+ *
+ * @param  {object}  service
+ * @return {Promise}
+ */
+export async function create({ name, status }) {
+  try {
+    return await
+      new Service({ name, status })
+        .save()
+        .then(service => service.refresh());
+  } catch (err) {
+    logger.error('Error inserting new service to database.', err);
+  }
+}
+
+/**
+ * Update a service.
+ *
+ * @param  {number|string}  id
+ * @param  {object}         service
+ * @return {Promise}
+ */
+export async function update(id, { name, status }) {
+  try {
+    return await
+      new Service({ id })
+        .save({ name, status })
+        .then(service => service.refresh());
+  } catch (err) {
+    logger.error('Error updating service in database', err);
+  }
+}
