56298d499ff393de95cbf4960f3c4f9ea3cf1f6a
Saugat Acharya <mesaugat@gmail.com>
2017-05-20 00:59:42 +0545

    Fix tests

--- src/utils/string.js
+++ src/utils/string.js
@@ -9,22 +9,24 @@ export function center(text, spaceLength) {
   let emptySpace = ' ';
 
   text = text.trim();
-  let textLength = text.length - 10; //  remove color font length
+  let textLength = text.length - 10;  // remove color font length
 
   if (textLength >= spaceLength) {
     return text;
   }
+
   let spaceContainer = '';
   let spaceRequired = (spaceLength - textLength) / 2;
 
   for (let i = 0; i < spaceRequired; i++) {
     spaceContainer = spaceContainer + emptySpace;
   }
+
   let result = spaceContainer + text + spaceContainer;
 
   if (result.length - 10 > spaceLength) {
     result = result.slice(0, -1);
   }
-  
+
   return result;
 }
--- test/services/string.test.js
+++ test/services/string.test.js
@@ -1,36 +1,35 @@
-import faker from 'faker';
 import { assert } from 'chai';
-import { center } from '../utils/string';
+import { center } from '../../src/utils/string';
 
 describe('center', () => {
   const width = 8;
-  let text = faker.random.word();
+  let text = 'something'; // using random text will generate different results
 
-  it('should return centerized stringn with proper padding for even width.', () => {
+  it('should return centerized string with proper padding for even width.', () => {
     let result = center(text, width);
 
-    assert.equal(result);
+    assert.equal(result, `     ${text}    `);
   });
 });
 
 describe('center', () => {
   const width = 7;
-  let text = faker.random.word();
+  let text = 'something';
 
-  it('should return centerized stringn with proper padding for odd width.', () => {
+  it('should return centerized string with proper padding for odd width.', () => {
     let result = center(text, width);
 
-    assert.equal(result);
+    assert.equal(result, `    ${text}    `);
   });
 });
 
 describe('center', () => {
-  const  width  = 0;
-  let text = faker.random.word();
+  const width = 0;
+  let text = 'somethings';
 
-  it('should return default text if width length is less than text length', () => {
+  it('should return default text if width length is less than text length.', () => {
     let result = center(text, width);
 
-    assert.equal(result);
+    assert.equal(result, text);
   });
 });
