3fafb57e2fa4578a150d711cb2a02291003a1694
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-06-02 10:33:27 +0545

    Pass Monitor.isStatusDifferent

--- test/monitoring/Monitor.isStatusDifferent.test.js
+++ test/monitoring/Monitor.isStatusDifferent.test.js
@@ -6,13 +6,13 @@ import { STATUS_UP, STATUS_DOWN } from '../../src/services/status';
 describe('Monitor.isStatusDifferent', () => {
   let monitor = new Monitor({ name: faker.random.word() });
 
-  monitor.status = STATUS_UP;
-
   it('should return true if status is different.', () => {
-    assert(monitor.isStatusDifferent(STATUS_DOWN));
+    monitor.status = STATUS_UP;
+    assert.isTrue(monitor.isStatusDifferent(STATUS_DOWN));
   });
 
   it('should return false if status is same.', () => {
-    assert(monitor.isStatusDifferent(STATUS_UP));
+    monitor.status = STATUS_UP;
+    assert.isFalse(monitor.isStatusDifferent(STATUS_UP));
   });
 });
