4fbf538627e7b86dc497ddbcafb00d1b40a1894e
Deepak Adhikari <deeps.adhi@gmail.com>
2017-06-26 08:08:25 +0545

    Add WebSocket config

--- src/services/notifier.js
+++ src/services/notifier.js
@@ -3,6 +3,7 @@ import * as email from './email';
 import * as twilio from './twilio';
 import * as hipchat from './hipchat';
 import logger from '../utils/logger';
+import * as websocket from './websocket';
 
 /**
  * Available Notifier services.
@@ -11,8 +12,17 @@ const notifiers = {
   slack,
   email,
   twilio,
-  hipchat
+  hipchat,
+  websocket
 };
+
+
+// Start WebSeocket server if it is enabled
+for (let [key, service] of Object.entries(notifiers)) {
+  if (key === 'websocket' && service.isEnabled()) {
+    service.start();
+  }
+}
 
 /**
  * Send the uptime notifcations to the user
