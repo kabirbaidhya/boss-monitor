3e27b97cb0606ebb5ccee2b9fdb37dadc3a63201
Deepak Adhikari <deeps.adhi@gmail.com>
2017-06-29 12:27:25 +0545

    Use websocket wrapper function

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -1,8 +1,5 @@
-import WebSocket from 'ws';
-import logger from '../utils/logger';
 import * as config from '../config/config';
-
-let webSocket;
+import * as wsServer from '../utils/websocket';
 
 /**
  * Check WebScoket notification is enabled.
@@ -16,56 +13,19 @@ export function isEnabled() {
 }
 
 /**
+ * Initialize WebSocket server.
+ */
+export function init() {
+  let { websocket } = config.get().notifications;
+
+  wsServer.init(websocket.port);
+}
+
+/**
  * Send notification through WebSocket.
  *
  * @param  {Object} params
  */
 export function notify(params) {
-  logger().debug('Notification Params:', params);
-
-  webSocket.broadcast = function broadcast(data) {
-    let message = {
-      data: data,
-      event: 'server-status-change'
-    };
-
-    message = JSON.stringify(message);
-
-    webSocket.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  };
-
-  webSocket.broadcast(params);
-}
-
-/**
- * Start WebSocket server.
- */
-export function start() {
-  let port = config.get().notifications.websocket.port;
-
-  if (webSocket) {
-    return;
-  }
-
-  webSocket =  new WebSocket.Server({ port: port });
-
-  logger().debug(`WebSocket started on port ${port}`);
-
-  let message = {
-    event: 'ws-connection-establish',
-    data: {
-      message: 'Chill WebSocket connection established'
-    }
-  };
-
-  message = JSON.stringify(message);
-
-  webSocket.on('connection', function connection(ws) {
-    ws.send(message);
-  });
-
+  wsServer.broadcast(params);
 }
