17594589d36f37de3b905dd30d467f86de8f7072
Saugat Acharya <mesaugat@gmail.com>
2017-07-11 11:04:19 +0545

    Update description

--- test/services/status.test.js
+++ test/services/status.test.js
@@ -2,16 +2,16 @@ import { assert } from 'chai';
 import * as status from '../../src/services/status';
 
 describe('status.getCheckInterval', () => {
-  let maxInterval = 10000;
   let minInterval = 1000;
+  let maxInterval = 10000;
 
-  it('should return the max. interval if status is up.', () => {
+  it('should return maximum interval if status is up.', () => {
     let result = status.getCheckInterval(status.STATUS_UP, minInterval, maxInterval);
 
     assert.equal(result, maxInterval);
   });
 
-  it('should return the min. interval if status is down.', () => {
+  it('should return minimum interval if status is down.', () => {
     let result = status.getCheckInterval(status.STATUS_DOWN, minInterval, maxInterval);
 
     assert.equal(result, minInterval);
