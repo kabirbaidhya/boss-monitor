2d272b625a39c56605d946690fb0c00f4c0e2475
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-12-21 19:39:00 +0545

    Use separate monitor instances for each tests

--- test/monitoring/Monitor.shouldRetry.test.js
+++ test/monitoring/Monitor.shouldRetry.test.js
@@ -4,25 +4,31 @@ import Monitor from '../../src/monitoring/Monitor';
 import { STATUS_UP, STATUS_DOWN } from '../../src/services/status';
 
 describe('Monitor.shouldRetry', () => {
-  let name = faker.random.word();
-  let monitor = new Monitor({ name });
-
   it('should return false if status is up.', () => {
-    let maxRetry = faker.random.number({ min: 3, max: 5 });
+    const name = faker.random.word();
+    const monitor = new Monitor({ name });
+
+    const maxRetry = faker.random.number({ min: 3, max: 5 });
 
     monitor.retried = maxRetry;
     assert.isFalse(monitor.shouldRetry(name, STATUS_UP, maxRetry));
   });
 
   it('should return false if status is down and retried count is greater than max retry', () => {
-    let maxRetry = faker.random.number({ min: 3, max: 5 });
+    const name = faker.random.word();
+    const monitor = new Monitor({ name });
+
+    const maxRetry = faker.random.number({ min: 3, max: 5 });
 
     monitor.retried = maxRetry + 1;
     assert.isFalse(monitor.shouldRetry(name, STATUS_DOWN, maxRetry));
   });
 
   it('should return true if status is down and retried count is less than max retry', () => {
-    let maxRetry = faker.random.number({ min: 3, max: 5 });
+    const name = faker.random.word();
+    const monitor = new Monitor({ name });
+
+    const maxRetry = faker.random.number({ min: 3, max: 5 });
 
     monitor.retried = maxRetry - 1;
     assert.isTrue(monitor.shouldRetry(name, STATUS_DOWN, maxRetry));
