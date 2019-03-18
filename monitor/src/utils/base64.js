29d7ba26397668a19b6a8a76dedf5353495cef0a
Shreejit <shreejit955@gmail.com>
2019-03-18 22:34:04 +0545

    add decode function to base64 util

--- monitor/src/utils/base64.js
+++ monitor/src/utils/base64.js
@@ -5,7 +5,15 @@
  * @returns {String}
  */
 export function encode(text) {
-  const buf = Buffer.from(text, 'ascii');
+  return Buffer.from(text, 'ascii').toString('base64');
+}
 
-  return buf.toString('base64');
+/**
+ * Decodes a given string to ascii string.
+ * 
+ * @param {String} text
+ * @returns {String} 
+ */
+export function decode(text) {
+  return Buffer.from(text, 'base64').toString('ascii');
 }
