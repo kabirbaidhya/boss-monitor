65157a45d592610a9eaf49e7552548223b2539b4
manisacharya <manisacharyaa@gmail.com>
2019-03-21 13:58:11 +0545

    Add HistoryPanel component

--- dashboard/src/components/dashboard/StatusPage.js
+++ dashboard/src/components/dashboard/StatusPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import StatusPanel from './StatusPanel';
+import HistoryPanel from './history/HistoryPanel';
 
 const StatusPage = () => (
   <div className="page-wrapper wrapper">
     <StatusPanel />
+    <HistoryPanel />
   </div>
 );
 
