b279ceef07408a0d77e299540dd8e75912d69d01
manisacharya <manisacharyaa@gmail.com>
2019-03-28 22:58:07 +0545

    Change HistoryRow to Event

---
+++ dashboard/src/components/dashboard/history/Event.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+
+import * as statuses from '../../../constants/statuses';
+
+const historyStatusParam = {
+  up: statuses.HISTORY_STATUS_UP_CLASS,
+  pending: statuses.HISTORY_STATUS_PENDING_CLASS,
+  down: statuses.HISTORY_STATUS_DOWN_CLASS
+};
+
+const Event = ({ data }) => {
+  const { id, createdAt, service, status } = data;
+
+  const serviceData = JSON.parse(service);
+  const statusData = JSON.parse(status);
+
+  const className = historyStatusParam[statusData.name.toLowerCase()];
+
+  return (
+    <ul className="status-update-list">
+      <li className={`status-update ${className}`}>
+        {serviceData.name} - <span className="state">{statusData.name} state</span> on
+        <span className="time"> {moment(createdAt).format('hh:mm A')}</span>
+      </li>
+    </ul>
+  );
+};
+
+Event.propTypes = {
+  data: PropTypes.object
+};
+
+export default Event;
