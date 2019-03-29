51615030c7bc748a102294682796de7c08d60199
manisacharya <manisacharyaa@gmail.com>
2019-03-29 09:40:55 +0545

    Remove # on the links

--- dashboard/src/components/commons/Header.js
+++ dashboard/src/components/commons/Header.js
@@ -7,13 +7,13 @@ const Header = ({ appStyle, projectStyle, appLogo, projectLogo }) => (
       <div className="d-flex align-items-center justify-content-between Header__row">
         <div className="Header__row__left-part d-flex align-items-center">
           <div className="Header__row__logo">
-            <a href="#">
+            <a href="">
               <img style={appStyle} src={appLogo.url} alt="Application Logo" />
             </a>
           </div>
         </div>
         <div className="Header__row__right-part d-flex align-items-center">
-          <a href="#">
+          <a href="">
           <img style={projectStyle} src={projectLogo.url} alt="Project Logo" />
           </a>
         </div>
