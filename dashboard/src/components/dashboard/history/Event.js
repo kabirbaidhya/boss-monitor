41de442bc8ee3f8eddd142a86a244219cc982c9a
manisacharya <manisacharyaa@gmail.com>
2019-03-29 09:40:28 +0545

    Change variable names

--- dashboard/src/components/dashboard/history/Event.js
+++ dashboard/src/components/dashboard/history/Event.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import * as statuses from '../../../constants/statuses';
 
-const historyStatusParam = {
+const eventStatusParam = {
   up: statuses.HISTORY_STATUS_UP_CLASS,
   pending: statuses.HISTORY_STATUS_PENDING_CLASS,
   down: statuses.HISTORY_STATUS_DOWN_CLASS
@@ -16,11 +16,11 @@ const Event = ({ data }) => {
   const serviceData = JSON.parse(service);
   const statusData = JSON.parse(status);
 
-  const className = historyStatusParam[statusData.name.toLowerCase()];
+  const eventStatus = eventStatusParam[statusData.name.toLowerCase()];
 
   return (
     <ul className="status-update-list">
-      <li className={`status-update ${className}`}>
+      <li className={`status-update ${eventStatus}`}>
         {serviceData.name} - <span className="state">{statusData.name} state</span> on
         <span className="time"> {moment(createdAt).format('hh:mm A')}</span>
       </li>
