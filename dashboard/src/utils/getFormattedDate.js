0c7cf53803d9829f2218ecc7c269cf601233f75d
manisacharya <manisacharyaa@gmail.com>
2019-04-12 23:46:34 +0545

    Added semicolons

--- dashboard/src/utils/getFormattedDate.js
+++ dashboard/src/utils/getFormattedDate.js
@@ -1,13 +1,12 @@
 import moment from 'moment';
 
-export function getFormattedDate (date, type) {
-
-  if (type === 'date'){
-    return moment(date).format("MMMM DD, YYYY, dddd")
+export function getFormattedDate(date, type) {
+  if (type === 'date') {
+    return moment(date).format('MMMM DD, YYYY, dddd');
   }
 
-  if (type === 'time'){
-    return moment(date).format('hh:mm A')
+  if (type === 'time') {
+    return moment(date).format('hh:mm A');
   }
 
   return moment(date).format();
