9d43885c32385facddca37c34fa5605963780c7d
Alesh Dulal <aleshdulal@lftechnology.com>
2019-08-14 13:40:20 +0545

    fix broken image link and  make ladder

--- dashboard/src/constants/icons.js
+++ dashboard/src/constants/icons.js
@@ -1,10 +1,10 @@
-import statusUpIcon from '../../public/images/icon-green-1.png';
-import statusDownIcon from '../../public/images/icon-red-1.png';
+import statusUpIcon from '../../public/images/success.png';
+import statusDownIcon from '../../public/images/cancel.png';
+import statusPendingIcon from '../../public/images/warning.png';
 import statusMaintenanceIcon from '../../public/images/status-maintenance.png';
-import statusPendingIcon from '../../public/images/icon-orange-1.png';
 
 export const UP = statusUpIcon;
 export const DOWN = statusDownIcon;
-export const UNDER_MAINTENANCE = statusMaintenanceIcon;
 export const PENDING = statusPendingIcon;
+export const UNDER_MAINTENANCE = statusMaintenanceIcon;
 
